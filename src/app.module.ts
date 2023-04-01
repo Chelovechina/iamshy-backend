9ba949452692f3897530a0dab714f0dcd8db27d8
Chelovechina <zkk2004@gmail.com>
2023-04-01 17:45:44 +0600

    init: intialize posts' and users' services, modules, controllers, schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { ControllerService } from './module/controller/controller.service';
+import { PostsService } from './posts/posts.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://root:cvNlc1v6Kr1FflnO@social.gwooslb.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+    PostsModule,
+  ],
+  controllers: [UsersController],
+  providers: [ControllerService, PostsService],
 })
-export class AppModule {}
+export class AppModule { }
